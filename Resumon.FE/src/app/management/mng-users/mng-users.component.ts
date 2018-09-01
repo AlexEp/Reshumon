5134e6c88cc709688ba43be6b8d1ea6c32a6add4
AlexEp <Alex.myreg@gmail.com>
2018-09-01 20:06:55 +0300

    Add ChartJs Library

--- Resumon.FE/src/app/management/mng-users/mng-users.component.ts
+++ Resumon.FE/src/app/management/mng-users/mng-users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-mng-users',
   templateUrl: './mng-users.component.html',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MngUsersComponent implements OnInit {
 
+  chart : any ; // This will hold our chart info
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+
 
 }
